2a7fe3d70bbc6981d797e29b15bd7f1b3e2f5b33
Nikos Roussos <nikos@roussos.cc>
2020-04-13 14:26:02 +0300

    Try google bucket

--- tasks/post-deploy/99-install-plugins.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gcloud source repos clone github_greenpeace_planet4-3rdparty-plugins --project=planet-4-151612
-
-wp plugin install --activate github_greenpeace_planet4-3rdparty-plugins/sitepress-multilingual-cms.4.3.4.zip
